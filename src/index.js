e6d618efbbdc2935afc652f932a2e5659b39fd31
Ema Romero López <emarlcoder@gmail.com>
2022-04-05 18:17:52 -0300

    Deploying backend

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('APP is running');
+})
+
+app.use('/posts', postRoutes);
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(port, () => console.log(`Server started on port ${port}`)))
+  .catch((error) => console.log(error.message));
